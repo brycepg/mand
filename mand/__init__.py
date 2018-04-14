60bea1cb7567c0364b10f12e8537df7af9731691
Bryce Guinta <bryce.paul.guinta@gmail.com>
2018-04-13 19:28:43 -0600

    Bump version

--- mand/__init__.py
+++ mand/__init__.py
@@ -1,7 +1,7 @@
 """Execute multiple modules in the same interpreter"""
 import sys
 
-__version__ = "0.9.5"
+__version__ = "0.9.6"
 if sys.version_info[0] < 3:
     raise Exception("Cannot run on python 2")
 
